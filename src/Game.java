634a79ff0d34b98a7e14c570de7d10fafb222ea1
crypticalfish86 <jasonweerawardena@gmail.com>
2024-11-16 19:19:59 +0000

    Game and GameSession partially set up

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,19 @@
+import java.io.File;
+
 public class Game {
+    private String loadedPlayerProfileID;
+    private GameSession currentGamesession;
+
+
+    //currentGameSession and loadedPlayerProfileID do not get instantiated in the constructor as they are variable (debatable whether anything gets instantiated in the constructor
+    Game() {
+    }
+
+    public void setPlayerProfile(String playerProfileID) {
+        this.loadedPlayerProfileID = playerProfileID;
+    }
+
+    public void loadGame(File levelFileData) {
+        currentGamesession = new GameSession(this, levelFileData.toString());//This is not how we will pass the level file data, it is just a placeholder for now
+    }
 }
--- src/GameSession.java
+++ src/GameSession.java
@@ -1,2 +1,38 @@
 public class GameSession {
+
+    private int gridHeight; //The height of the grid
+    private int gridWidth; //The width of the grid
+
+    private long startTimeStamp; //The starting time stamp (ms since 1970)
+    private long maxTimeToCompleteLevel; //The time given to complete level (in ms)
+
+    private Tile[][] gridTileMap; //The full 2D grid instantiated on the interpretation of the level data
+    private final Game game; // Reference to the game that the current game session is attached to
+    private final GameSessionData currentSessionData; //Reference to this games' game session data
+    private final Player gamePlayerTile; //Reference to the current single game player (inserted into the level in "interpretLevelData"
+    GameSession(Game game, String gameData) {
+        this.game = game;
+        this.currentSessionData = new GameSessionData();
+        this.gamePlayerTile = new Player();
+        interpretLevelData(gameData);
+    }
+
+    /*
+        method used to interpret the gameData (from a levelFileFormat file) fills the gridTileMap, initiates timer,
+        determine the
+    */
+    private void interpretLevelData(String gameData) {
+        return;
+    }
+
+    //returns a specific tile from the gridTileMap
+    public Tile getTileFromGrid(int x, int y) {
+        return gridTileMap[y][x];
+    }
+
+    //updates the incoming tile position with the replacement tile and the outgoing tile with the incoming tile
+    public void updateTilePositions(Tile replacementTile, Tile incomingTile, Tile outgoingTile) {
+        return;
+    }
+
 }
