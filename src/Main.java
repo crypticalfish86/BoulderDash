93e2fcaf053baa189ec1244ddf39efb0506878a7
crypticalfish86 <jasonweerawardena@gmail.com>
2024-11-18 16:12:35 +0000

    changed HelloFX to main class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class HelloFX extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
