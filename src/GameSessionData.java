d143277bb4a412e510fd7228e83c323713050aba
Omarzarugh <2141745@swansea.ac.uk>
2024-11-21 13:10:22 +0000

    GameSessionData fixed.

--- src/GameSessionData.java
+++ src/GameSessionData.java
@@ -6,21 +6,41 @@ public class GameSessionData {
     private int diamondCount;
     private int score;
 
-    public static GameSessionData createGameSessionData() {
-        return new GameSessionData();
-    }
+    /*public GameSessionData(GameSession GameSession) {
 
-    public int getInventoryItem() {
-        return 0;
-    }
+    }*/
+    //TODO fix why this makes GameSession cry
 
-    public void UpdateInventoryItem() {
-    }
+    public GameSessionData() {
 
+    }
+    public void getInventoryItem(String item) {
+
+    }
+    public void updateInventory(String item, boolean increment) {
+
+    }
     public int getScore() {
-        return 0;
+        return score;
+    }
+    public void updateScore(int scoreToAdd) {
+
     }
 
-    public void updateScore() {
-    }
+    /*
+        public static GameSessionData createGameSessionData() {
+        return new GameSessionData();
+        }
+        public int getInventoryItem() {
+        return 0;
+        }
+        public void UpdateInventoryItem() {
+        }
+        public int getScore() {
+        return 0;
+        }
+        public void updateScore() {
+        }
+        */
+
 }
