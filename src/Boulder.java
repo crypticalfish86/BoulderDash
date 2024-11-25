5b08f687f9d70a97e4d862ba7a9a1d9c9d364a16
IsaacA27 <iatkinson18@outlook.com>
2024-11-25 01:58:24 +0000

    add JavaDoc in boulder

--- src/Boulder.java
+++ src/Boulder.java
@@ -13,6 +13,14 @@ public class Boulder extends FallingObject{
         this.amoebaCanSpreadToThisTile = false;
     }
 
+
+    /**
+     * Handles pushing the boulder based on the
+     * player's position relative to the boulder and
+     * whether there is a path in the direction the boulder
+     * is being pushed
+     * @param tile the tile that is interacting with this tile
+     */
     public void interact(Tile tile){
         if(tile.getTileType() == TileType.PLAYER){
 
@@ -71,6 +79,12 @@ public class Boulder extends FallingObject{
         }
     }
 
+
+    /**
+     * Pushes the boulder in a specified direction
+     * @param player the player that is pushing the boulder
+     * @param direction the direction the boulder is being pushed
+     */
     private void pushBoulder(Tile player, String direction){
         int offset;
         switch (direction){
