a2961c1de0862af1a5afbcd532bc3403f0b7e71f
cmcoff <148201231+cmcoff@users.noreply.github.com>
2024-12-09 03:45:23 +0000

    Player javadoc

--- src/Player.java
+++ src/Player.java
@@ -1,170 +1,121 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
-
-
-
+/**
+ * Represents the player in the game, inheriting from the Tile class.
+ * The player can move, interact with tiles, and respond to input.
+ * @author Alex (Tsz Tung Yee)
+ * @author Cameron McDonald (cmcoff)
+ *  * @version 2.1
+ */
 public class Player extends Tile {
 
-    public static final Image img = new Image("file:Assets/Images/PlayerForward.png"); // Placeholder for the image
+    /**
+     * The image used to represent the player.
+     */
+    public static final Image img = new Image("file:Assets/Images/PlayerForward.png");
 
+    // Directional keys tracking
     private boolean keyUp = false;
     private boolean keyDown = false;
     private boolean keyLeft = false;
     private boolean keyRight = false;
 
-    //either stores W, S, A, D or space (no input currently)
+    // Current and last direction of movement
     private char currentDirection = ' ';
     private char lastDirection = ' ';
+
+    // Frame count for movement speed adjustment
     private int recurringFrameCount = 0;
 
+    // Constants for movement speed
     public static final int RECURRING_DIRECTION_SPEED_UP = 4;
     public static final int RECURRING_DIRECTION_INITIAL_SPEED = 20;
-    public static final int RECURRING_DIRECTION_FAST_SPEED = 7; // in frames
+    public static final int RECURRING_DIRECTION_FAST_SPEED = 7;
 
-    
-
+    /**
+     * Constructs a Player object with the position and operation interval listed.
+     *
+     * @param gameSession       The game session to which the player is in.
+     * @param x                 The initial x-coordinate of the player.
+     * @param y                 The initial y-coordinate of the player.
+     * @param operationInterval The interval between tile updates.
+     */
     public Player(GameSession gameSession, int x, int y, long operationInterval) {
         super(gameSession, x, y, TileType.PLAYER, operationInterval);
-        // Initialize the inventory, not needed because you should call the game session data, and key should decide the logic instead of the player
     }
 
-
-
-
-
-
-    // Handle player input (example)
+    /**
+     * Handles input and player movement
+     *
+     * @param key The key pressed by the player ('W', 'A', 'S', 'D').
+     */
     public void onKeyPressed(char key) {
-        
-        // setting these boolean allows us to check which key the player is inputting
         switch (key) {
-            case 'W':
-                keyUp = true;
-                currentDirection = 'W';
-                break;
-            case 'S':
-                keyDown = true;
-                currentDirection = 'S';
-                break;
-            case 'A':
-                keyLeft = true;
-                currentDirection = 'A';
-                break;
-            case 'D':
-                keyRight = true;
-                currentDirection = 'D';
-                break;
-            default:
-                System.out.printf("Warning: input down not supported in player: %c\n", key);
+            case 'W': keyUp = true; currentDirection = 'W'; break;
+            case 'S': keyDown = true; currentDirection = 'S'; break;
+            case 'A': keyLeft = true; currentDirection = 'A'; break;
+            case 'D': keyRight = true; currentDirection = 'D'; break;
+            default: System.out.printf("Warning: Unsupported key pressed: %c\n", key);
         }
     }
 
+    /**
+     * adjusts players direction based on key release
+     *
+     * @param key The key released by the player.
+     */
     public void onKeyReleased(char key) {
-
-        // setting these boolean allows us to check which key the player is inputting
         switch (key) {
-            case 'W':
-                keyUp = false;
-                break;
-            case 'S':
-                keyDown = false;
-                break;
-            case 'A':
-                keyLeft = false;
-                break;
-            case 'D':
-                keyRight = false;
-                break;
-            default:
-                System.out.printf("Warning: input up not supported in player: %c\n", key);
+            case 'W': keyUp = false; break;
+            case 'S': keyDown = false; break;
+            case 'A': keyLeft = false; break;
+            case 'D': keyRight = false; break;
+            default: System.out.printf("Warning: Unsupported key released: %c\n", key);
         }
 
-        //find out which key is still down
-        if (keyUp) {
-            currentDirection = 'W';
-        } else if (keyDown) {
-            currentDirection = 'S';
-        } else if (keyLeft) {
-            currentDirection = 'A';
-        } else if (keyRight) {
-            currentDirection = 'D';
-        } else {
-            currentDirection = ' ';
-        }
-
+        // Determine the new direction
+        if (keyUp) currentDirection = 'W';
+        else if (keyDown) currentDirection = 'S';
+        else if (keyLeft) currentDirection = 'A';
+        else if (keyRight) currentDirection = 'D';
+        else currentDirection = ' ';
     }
 
     @Override
     public void interact(Tile tile) {
-        if((tile.getTileType() == TileType.BOULDER || tile.getTileType() == TileType.DIAMOND)) {
-            PathWall pathWall = new PathWall(gameSession, this.x, this.y + 1, operationInterval );
-            gameSession.updateTilePositions(pathWall, tile, this);
-            this.killPlayer();
+        if (tile.getTileType() == TileType.BOULDER || tile.getTileType() == TileType.DIAMOND) {
+            gameSession.updateTilePositions(new PathWall(gameSession, this.x, this.y + 1, operationInterval), tile, this);
+            killPlayer();
+        } else if (tile.getTileType() == TileType.MOVING_ENEMY) {
+            gameSession.updateTilePositions(new PathWall(gameSession, this.x, this.y + 1, operationInterval), tile, this);
+            killPlayer();
         }
-        else if(tile.getTileType() == TileType.MOVING_ENEMY){
-            PathWall pathWall = new PathWall(gameSession, this.x, this.y + 1, operationInterval );
-            gameSession.updateTilePositions(pathWall, tile, this);
-            this.killPlayer();
-        }
-
-
     }
 
     @Override
     public void updateTile(long currentTimeInMilliseconds) {
-        if (currentDirection != lastDirection) {
-            //resets the counter
-            recurringFrameCount = 0;
+        if (currentDirection != lastDirection) recurringFrameCount = 0;
+        else recurringFrameCount++;
 
-        } else {
-            ++recurringFrameCount;
-
-        }
         lastDirection = currentDirection;
 
-        int framesRequiredToSpeedUp = RECURRING_DIRECTION_INITIAL_SPEED * RECURRING_DIRECTION_SPEED_UP;
-        boolean shouldMove;
-        if (recurringFrameCount < framesRequiredToSpeedUp) {
-            shouldMove = recurringFrameCount % RECURRING_DIRECTION_INITIAL_SPEED == 0;
-        } else {
-            shouldMove = (recurringFrameCount - framesRequiredToSpeedUp) % RECURRING_DIRECTION_FAST_SPEED == 0;
-        }
+        boolean shouldMove = recurringFrameCount < RECURRING_DIRECTION_INITIAL_SPEED * RECURRING_DIRECTION_SPEED_UP
+                ? recurringFrameCount % RECURRING_DIRECTION_INITIAL_SPEED == 0
+                : (recurringFrameCount - RECURRING_DIRECTION_INITIAL_SPEED * RECURRING_DIRECTION_SPEED_UP) % RECURRING_DIRECTION_FAST_SPEED == 0;
 
-        
-        if (!shouldMove) { return; } // if this tile should not move then nothing happens
+        if (!shouldMove) return;
 
-
-        //interact with the respective direction
-        
+        // Interact with the tile in the current direction
         switch (currentDirection) {
-            case 'W':
-                //interact with the tile on the right
-                gameSession.getTileFromGrid(this.getXPosition(), this.getYPosition() - 1).interact(this);
-                break;
-                
-            case 'S':
-                //interact with the tile on the right
-                gameSession.getTileFromGrid(this.getXPosition(), this.getYPosition() + 1).interact(this);
-                break;
-            
-            case 'A':
-                //interact with the tile on the right
-                gameSession.getTileFromGrid(this.getXPosition() - 1, this.getYPosition()).interact(this);
-                break;
-                
-            case 'D':
-                //interact with the tile on the right
-                gameSession.getTileFromGrid(this.getXPosition() + 1, this.getYPosition()).interact(this);
-                break;
+            case 'W': gameSession.getTileFromGrid(this.getXPosition(), this.getYPosition() - 1).interact(this); break;
+            case 'S': gameSession.getTileFromGrid(this.getXPosition(), this.getYPosition() + 1).interact(this); break;
+            case 'A': gameSession.getTileFromGrid(this.getXPosition() - 1, this.getYPosition()).interact(this); break;
+            case 'D': gameSession.getTileFromGrid(this.getXPosition() + 1, this.getYPosition()).interact(this); break;
         }
 
-
-        //Kill player if they next to an enemy
-        if(isNextToEnemy()){
-            this.killPlayer();
-        }
-        
+        // Kill player if an enemy is nearby
+        if (isNextToEnemy()) killPlayer();
     }
 
     @Override
@@ -172,44 +123,31 @@ public class Player extends Tile {
         draw(gc, img, 0, 0);
     }
 
-
-
-    // Add the killPlayer method
+    /**
+     * Ends the game when the player is killed.
+     */
     public void killPlayer() {
         gameSession.onGameOver(false);
     }
 
     /**
-     * Determines if an enemy is next to the player in any of the four cardinal directions
-     * @return true if there is an enemy next to the player, false otherwise
+     * Checks if there is an enemy next to the player.
+     *
+     * @return true if an enemy is nearby; false otherwise.
      */
-    private boolean isNextToEnemy(){
-        if(this.x != 0){
-            if(gameSession.getTileFromGrid(this.x - 1, this.y).getTileType() == TileType.MOVING_ENEMY){
-                return true;
-            }
-        }
-        if(this.x != gameSession.getGridWidth() - 1){
-            if(gameSession.getTileFromGrid(this.x + 1, this.y).getTileType() == TileType.MOVING_ENEMY){
-                return true;
-            }
-        }
-        if(this.y != 0){
-            if(gameSession.getTileFromGrid(this.x, this.y - 1).getTileType() == TileType.MOVING_ENEMY){
-                return true;
-            }
-        }
-        if(this.y != gameSession.getGridHeight() - 1){
-            if (gameSession.getTileFromGrid(this.x, this.y + 1).getTileType() == TileType.MOVING_ENEMY){
-                return true;
-            }
-        }
-
-        return false;
+    private boolean isNextToEnemy() {
+        return gameSession.getTileFromGrid(this.x - 1, this.y).getTileType() == TileType.MOVING_ENEMY ||
+                gameSession.getTileFromGrid(this.x + 1, this.y).getTileType() == TileType.MOVING_ENEMY ||
+                gameSession.getTileFromGrid(this.x, this.y - 1).getTileType() == TileType.MOVING_ENEMY ||
+                gameSession.getTileFromGrid(this.x, this.y + 1).getTileType() == TileType.MOVING_ENEMY;
     }
 
-
-    public String returnStringTileRepresentation(){
+    /**
+     * Returns the player's string representation for save states.
+     *
+     * @return "P" as the player's string representation.
+     */
+    public String returnStringTileRepresentation() {
         return "P";
     }
 }
