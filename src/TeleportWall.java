6d1c4584cbefd1b3c19cebfbeaec1835e6fb5c30
crypticalfish86 <jasonweerawardena@gmail.com>
2024-12-09 02:08:51 +0000

    added level 1, level 5 and changes to teleport tile.

--- src/TeleportWall.java
+++ src/TeleportWall.java
@@ -94,13 +94,13 @@ public class TeleportWall extends Wall{
         Tile southTile = this.gameSession.getTileFromGrid(this.x, this.y - 1);
         Tile westTile = this.gameSession.getTileFromGrid(this.x - 1, this.y);
         if(northTile.getTileType() == TileType.PATH){
-            tileToTeleportPlayerTo = this.gameSession.getTileFromGrid(this.x + 1, this.y);
-        } else if(eastTile.getTileType() == TileType.PATH){
-            tileToTeleportPlayerTo = this.gameSession.getTileFromGrid(this.x - 1, this.y);
-        } else if(southTile.getTileType() == TileType.PATH){
             tileToTeleportPlayerTo = this.gameSession.getTileFromGrid(this.x, this.y + 1);
-        } else if(westTile.getTileType() == TileType.PATH){
+        } else if(eastTile.getTileType() == TileType.PATH){
+            tileToTeleportPlayerTo = this.gameSession.getTileFromGrid(this.x + 1, this.y);
+        } else if(southTile.getTileType() == TileType.PATH){
             tileToTeleportPlayerTo = this.gameSession.getTileFromGrid(this.x, this.y - 1);
+        } else if(westTile.getTileType() == TileType.PATH){
+            tileToTeleportPlayerTo = this.gameSession.getTileFromGrid(this.x - 1, this.y);
         }
 
         if(tileToTeleportPlayerTo != null){
